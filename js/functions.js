7d2022bfe7ca66d0b58dc2ce16c80dcf9d5ba9ef
Lukchai Pattarakijkomjorn <lukchai@gmail.com>
2012-10-02 09:29:30 +0700

    My initial commit message

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+$(function() {
+	$(document).on('focusin', '.field, textarea', function() {
+		if(this.title==this.value) {
+			this.value = '';
+		}
+	}).on('focusout', '.field, textarea', function(){
+		if(this.value=='') {
+			this.value = this.title;
+		}
+	});
+
+	$('#navigation ul li:first-child').addClass('first');
+	$('.footer-nav ul li:first-child').addClass('first');
+
+	$('#navigation a.nav-btn').click(function(){
+		$(this).closest('#navigation').find('ul').slideToggle()
+		$(this).find('span').toggleClass('active')
+		return false;
+	})
+});
+
+$(window).load(function() {
+	$('.flexslider').flexslider({
+		animation: "slide",
+		controlsContainer: ".slider-holder",
+		slideshowSpeed: 5000,
+		directionNav: true,
+		controlNav: true,
+		animationDuration: 1000,
+
+	});
+});
